857bd3fd21be3e31ff80a3052705ffbc43ba90d0
Eugene T <41307970+e-src@users.noreply.github.com>
2020-11-23 12:11:07 -0800

    Add initial API tests

---
+++ cypress/integration/API/registration/registration.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+Cypress.config('baseUrl', Cypress.config('API_ROOT'));
+
+describe('Registration endpoint', () => {
+
+  before(() => {
+    cy.resetDB();
+  })
+
+  it('successfully registers a user', () => {
+    cy.fixture('registration/user-karen').then((fx) => {
+        cy.request({
+            method: 'POST',
+            url: '/users',
+            body: fx
+        }).then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.user.username).to.equal(fx.user.username);
+            expect(response.body.user.token).not.to.be.empty;
+        })
+    })
+  });
+
+  it('verifies responses for incomplete requests', () => {
+    // showcase of a data driven test
+    cy.fixture('registration/users-incomplete').then((fixture) => {
+      cy.wrap(fixture)
+        .each(fx => {
+          cy.request({
+            method: 'POST',
+            url: '/users',
+            body: fx.request,
+            failOnStatusCode: false
+          }).then((response) => {
+            expect(response.status).to.equal(fx.status);
+            expect(response.body).to.deep.equal(fx.response);
+          })
+        })
+    })
+    
+  });
+
+
+
+})
